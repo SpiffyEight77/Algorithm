02027b110fcdb3bc0a3737be3d1156bfaca2e156
SpiffyEight77 <killer_0248@live.com>
2017-03-13 22:32:55 +0800

    A + B Problem

---
+++ Java/HDU1000.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+        public static void main(String[] argc) {
+            int a, b;
+            Scanner cin = new Scanner(System.in);
+            while (cin.hasNext())
+            {
+                a = cin.nextInt();
+                b = cin.nextInt();
+                System.out.println(a+b);
+            }
+        }
+    }
