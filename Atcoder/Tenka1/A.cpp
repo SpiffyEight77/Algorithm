e620bb45ec275f951c0ec753c19b4beb722feee5
SpiffyEight77 <killer_0248@live.com>
2017-09-30 20:29:18 +0800

    Tenka1

--- Atcoder/Tenka1/A.cpp
+++ Atcoder/Tenka1/A.cpp
@@ -3,8 +3,16 @@
 #include<cstring>
 #include<cmath>
 using namespace std;
+const int M = 10010;
+char str[M];
+int ans;
 int main()
 {
-    
+    ans = 0;
+    scanf("%s",str);
+    for (int i = 0; i < strlen(str); i++)
+        if(str[i] == '1')
+            ans++;
+    printf("%d\n",ans);
     return 0;
 }
--- Atcoder/Tenka1/C.cpp
+++ Atcoder/Tenka1/C.cpp
@@ -3,8 +3,12 @@
 #include<cstring>
 #include<cmath>
 using namespace std;
+int N,h,n,w;
 int main()
 {
-    
+    scanf("%d",&N);
+    N/=2;
+    for (int i = 1; ; i++)
+        
     return 0;
 }
