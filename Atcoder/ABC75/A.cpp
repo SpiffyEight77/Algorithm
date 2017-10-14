4c36852f21dedd7b329772dc04a695056ce0832d
SpiffyEight77 <killer_0248@live.com>
2017-10-14 20:16:21 +0800

    Update

--- Atcoder/ABC75/A.cpp
+++ Atcoder/ABC75/A.cpp
@@ -4,7 +4,16 @@
 #include<algorithm>
 #include<cmath>
 using namespace std;
+int a,b,c;
 int main()
 {
+    scanf("%d%d%d",&a,&b,&c);
+    if(a == b)
+        printf("%d\n",c);
+    else
+        if(a == c)
+            printf("%d\n",b);
+    else
+        printf("%d\n",a);
     return 0;
 }
