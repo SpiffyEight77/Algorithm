62d5ef74da8f47d632d6a99f3b588eef6f9a2052
SpiffyEight77 <killer_0248@live.com>
2017-10-22 20:53:20 +0800

    Update

--- Atcoder/Code Festival 2017 qual C/A.cpp
+++ Atcoder/Code Festival 2017 qual C/A.cpp
@@ -5,8 +5,23 @@
 #include<cmath>
 using namespace std;
 const int Maxn = 100000;
+char s[Maxn];
+int l;
+bool f;
 int main()
 {
-
+    f = false;
+    scanf("%s",s);
+    l = strlen(s);
+    for (int i = 0; i < l; i++)
+        if(s[i] == 'A' && s[i+1] == 'C')
+        {
+            f = true;
+            break;
+        }
+    if(f)
+        printf("Yes\n");
+    else
+        printf("No\n");
     return 0;
 }
--- Atcoder/Code Festival 2017 qual C/B.cpp
+++ Atcoder/Code Festival 2017 qual C/B.cpp
@@ -5,8 +5,24 @@
 #include<cmath>
 using namespace std;
 const int Maxn = 100000;
+int n,k,q,p,ans,a[Maxn];
 int main()
 {
+    ans = 1;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+        scanf("%d",&a[i]);
 
+    for (int i = 0; i < n; i++)
+    {
+        k = 0;
+        for (int j = a[i]-1; j <= a[i]+1; j++)
+        {
+            if(j%2!=0)
+                k++;
+        }    
+        ans*=k;
+    }
+    printf("%0.lf\n",pow(3,n)-ans);
     return 0;
 }
