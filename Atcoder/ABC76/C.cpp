bce729e57bd41d9c4a0842457a4222a904d509cd
SpiffyEight77 <killer_0248@live.com>
2017-10-28 20:37:45 +0800

    Dubious Document 2

--- Atcoder/ABC76/C.cpp
+++ Atcoder/ABC76/C.cpp
@@ -6,12 +6,41 @@
 using namespace std;
 const int maxn = 100000;
 char s[maxn],t[maxn];
+int l1,l2,l3,k,i,j,p;
+bool f;
 int main()
 {
     scanf("%s",s);
     getchar();
     scanf("%s",t);
     getchar();
-    
+    l1 = strlen(s);
+    l2 = strlen(t);
+    l3 = l2;
+    k = 0;
+    f = true;
+    for (i = 0; i < l1; i++)
+    {
+        if(s[i] == t[k])
+        {
+            p = i;
+            for (j = i+1; j < l1; j++)
+                if(s[j] == t[k+1] || s[j] == '?')
+                    k++;
+                else
+                    break;
+        }
+        else
+            if(s[i] == '?')
+                s[i] = 'a';
+    }
+    if(k+1 != l2)
+        printf("UNRESTORABLE\n");
+    else
+    {
+        for (int i = 0; i < p; i++)
+            printf("%c",s[i]);
+        printf("%s\n",t);
+    }
     return 0;
 }
