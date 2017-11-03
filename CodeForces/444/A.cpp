dbc729eb3c0fa18ea3798843c39018a83d96f9bd
SpiffyEight77 <killer_0248@live.com>
2017-11-04 01:10:43 +0800

    CodeForces #444 Div.2

--- CodeForces/444/A.cpp
+++ CodeForces/444/A.cpp
@@ -3,10 +3,39 @@
 #include<algorithm>
 #include<cmath>
 #include<cstring>
+#define ans 1000000
 using namespace std;
 const int maxn = 100000;
+string S;
+int cnt;
+bool f;
 int main()
 {
-
+    while(cin>>S)
+    {
+        f = false;
+        cnt = 0;
+        for (int i = 0; i < S.size(); i++)
+        {
+            if(S[i] != '0')
+            {
+                for (int j = i+1; j < S.size(); j++)
+                {
+                    if(S[j] == '1')
+                    {
+                        cnt++;
+                        S[j] = '0';
+                    }
+                }
+                f = true;
+            }
+            if(f)
+                break;
+        }
+        if(S.size() - cnt >= 7)
+            cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+    }
     return 0;
 }
