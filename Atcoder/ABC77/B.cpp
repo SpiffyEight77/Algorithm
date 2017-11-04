de5ec05c3574e3310fb90d6b43e79f7369920595
SpiffyEight77 <killer_0248@live.com>
2017-11-04 21:30:42 +0800

    ABC77

--- Atcoder/ABC77/B.cpp
+++ Atcoder/ABC77/B.cpp
@@ -5,7 +5,27 @@
 #include<cmath>
 using namespace std;
 const int maxn = 100000;
+long long x,y,Max;
+bool f;
 int main()
 {
+    cin>>x;
+    Max = 0;
+    f = false;
+    for (long long i = 1; i <= x; i++)
+    {
+        y = i * i;
+        if(y > x)
+        {
+            cout<<Max<<endl;
+            f = true; 
+            break;
+        }
+        else
+            if(y >= Max)
+                Max = y;
+    }
+    if(!f)
+        cout<<Max<<endl;
     return 0;
 }
--- Atcoder/ABC77/C.cpp
+++ Atcoder/ABC77/C.cpp
@@ -4,8 +4,18 @@
 #include<algorithm>
 #include<cmath>
 using namespace std;
-const int maxn = 100000;
+const int maxn = 100100;
+int n,cnt,a[4][maxn],b[maxn],c[maxn];
+void dfs()
+{
+    
+}
 int main()
 {
+    cin>>n;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < n; j++)
+            cin>>a[i][j];
+       
     return 0;
 }
