4491f0feb171df24b9413e2b0b9b0da6b37c0c5a
SpiffyEight77 <killer_0248@live.com>
2017-11-18 20:37:44 +0800

    Update

--- Atcoder/ABC79/A.cpp
+++ Atcoder/ABC79/A.cpp
@@ -5,8 +5,13 @@
 #include<algorithm>
 using namespace std;
 const int Maxn = 100000;
+string s;
 int main()
 {
-
+    cin>>s;
+    if( (s[0] == s[1] && s[1] == s[2]) || (s[1] == s[2] && s[2] == s[3]) )
+        printf("Yes\n");
+    else
+        printf("No\n");
     return 0;
 }
--- Atcoder/ABC79/B.cpp
+++ Atcoder/ABC79/B.cpp
@@ -5,8 +5,15 @@
 #include<algorithm>
 using namespace std;
 const int Maxn = 100000;
+long long L[100];
+int n;
 int main()
 {
-
+    cin>>n;
+    L[0] = 2;
+    L[1] = 1;
+    for (int i = 2; i <= n; i++)
+        L[i] = L[i-1] + L[i-2];
+    cout<<L[n]<<endl;
     return 0;
 }
