c3feeabb33f43f31df007e6c854f84e5f9fab13c
SpiffyEight77 <killer_0248@live.com>
2017-11-24 13:28:02 +0800

    Ac2684

---
+++ Atcoder/Ac2684.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+const int Maxn = 100000;
+int n,m;
+int main()
+{
+    cin>>n>>m;
+    cout<<(n-1)*(m-1)<<endl;
+    return 0;
+}
