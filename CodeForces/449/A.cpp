84c53ecd4a83041254c48f1fcbd28577fe52715c
SpiffyEight77 <killer_0248@live.com>
2017-12-02 23:28:42 +0800

    Update

---
+++ CodeForces/449/A.cpp
@@ -0,0 +1,47 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+const int Maxn = 1000100;
+int k,p,n,m,l,res,a[10];
+long long ans;
+bool f;
+int main()
+{
+    ans = 0;
+    res = 0;
+    cin>>k>>p;
+    for (int i = 11; i <= Maxn; i+=11)
+    {
+        n = i;
+        l = 0;
+        f = true;
+        while(n!=0)
+        {
+            a[l++] = n % 10;
+            n/=10;
+        }
+        if(l%2!=0)
+            break;
+        for (int j = 0; j < l/2; j++)
+            if(a[j] != a[l-j-1])
+            {
+                f = false;
+                break;
+            }
+        if(f)
+        {
+            //cout<<i<<endl;
+            ans+=i;
+            res++;
+        }
+        if(res == k)
+        {
+            cout<<ans%p<<endl;
+            break;
+        }
+    }
+    return 0;
+}
