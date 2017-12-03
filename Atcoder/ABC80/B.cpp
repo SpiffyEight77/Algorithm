b29432bff7f07dc7b14810e30accaede10dcab46
SpiffyEight77 <killer_0248@live.com>
2017-12-03 19:56:47 +0800

    ABC80

---
+++ Atcoder/ABC80/B.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+using namepace std;
+const int Maxn = 100010;
+typedef long long ll;
+int main()
+{
+
+    return 0;
+}
