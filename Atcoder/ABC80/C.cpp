36bd25c4a7d0a4379c3830941b719aaef0ab7189
SpiffyEight77 <killer_0248@live.com>
2017-12-04 21:36:36 +0800

    Update

--- Atcoder/ABC80/C.cpp
+++ Atcoder/ABC80/C.cpp
@@ -6,22 +6,26 @@
 using namespace std;
 const int Maxn = 100010;
 typedef long long ll;
-int f[110][6][3],p[110][12],a[110],ans,res,n;
+int f[110][20],p[110][20],ans,res,n;
 int main()
 {
     cin>>n;
-    memset(a,0,sizeof(a));
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= 10; j++)
-        {
-            scanf("%d %d",&f[i][j][1],&f[i][j][2]);
-            if(a[i] == 1)
-                a[i]++;
-        }
-    for (int i = 1; i <= n; i++)
-        for (int j = 0; j <= 10; j++)
-            scanf("%d",&p[i][j]);
-    for (int i = 1; i <= n; i++)
-        
+
+    cout<<(n>>2)<<endl;
+    cout<<((n>>2) & 1)<<endl;
+    // for (int i = 1; i <= n; i++)
+    //     for (int j = 1; j <= 10; j++)
+    //         cin>>f[i][j];
+    // for (int i = 1; i <= n; i++)
+    //     for (int j = 0; j <= 10; j++)
+    //         cin>>p[i][j];
+    // res = -(1<<30);
+    // for (int k = 1; k <= 1024; k++)
+    // {
+    //     for (int i = 1; i <= n; i++)
+    //     {
+    //         for (int j = 0; j < 10)
+    //     }
+    // }
     return 0;
 }
