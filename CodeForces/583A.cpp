832a6f83b0cc4cac32dac2772a985178444bd482
SpiffyEight77 <killer_0248@live.com>
2017-12-10 19:32:53 +0800

    Update

---
+++ CodeForces/583A.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+const int Maxn = 100000;
+const int Mod = 1000000000 + 7;
+int main()
+{
+    
+    return 0;
+}
