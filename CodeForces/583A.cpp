08658f88b80421cf8c6b890c9071a16090f11332
SpiffyEight77 <killer_0248@live.com>
2017-12-10 19:57:19 +0800

    583A

--- CodeForces/583A.cpp
+++ CodeForces/583A.cpp
@@ -7,8 +7,20 @@ using namespace std;
 typedef long long ll;
 const int Maxn = 100000;
 const int Mod = 1000000000 + 7;
+int n,x,y,v[55],h[55];
 int main()
 {
-    
+    cin>>n;
+    for (int i = 1; i <= n * n; i++)
+    {
+        cin>>x>>y;
+        if(!v[y] && !h[x])
+        {
+            v[y] = 1;
+            h[x] = 1;
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
     return 0;
 }
