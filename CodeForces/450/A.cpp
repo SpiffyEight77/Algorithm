d122153095d5faa0c2b0ecf936348ce0d2aca21e
SpiffyEight77 <killer_0248@live.com>
2017-12-12 01:29:20 +0800

    update

--- CodeForces/450/A.cpp
+++ CodeForces/450/A.cpp
@@ -7,9 +7,30 @@ using namespace std;
 typedef long long ll;
 const int Maxn = 100000;
 const int Mod = 1000000000 + 7;
-int x,y;
+int n,x[Maxn],y[Maxn],l,r;
 int main()
 {
-    
+    cin>>n;
+    l = 0;
+    r = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>x[i]>>y[i];
+        if(x[i] > 0 && y[i] > 0)
+            r++;
+        
+        if(x[i] > 0 && y[i] < 0)
+            r++;
+        
+        if(x[i] < 0 && y[i] < 0)
+            l++;
+
+        if(x[i] < 0 && y[i] > 0)
+            l++;
+    }
+    if( (r  > 0 && l - 1 == 0) || (l  > 0 && r - 1 == 0) || (r - 1 > 0 && l == 0) || (l - 1 > 0 && r == 0)) 
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
     return 0;
 }
--- CodeForces/450/B.cpp
+++ CodeForces/450/B.cpp
@@ -1,15 +1,53 @@
 #include<cstdio>
 #include<iostream>
+#include <stdlib.h>  
 #include<cstring>
 #include<algorithm>
 #include<cmath>
 using namespace std;
 typedef long long ll;
-const int Maxn = 100000;
-const int Mod = 1000000000 + 7;
-int x,y;
+const int Maxn = 100500;
+const int Mod = 1000000000;
+int x,y,k,b;
+double a,c;
+char s[Maxn];
+bool f;
 int main()
 {
+    cin>>a>>b>>c;
+    //cout<<a<<endl;
+
+    sprintf(s, "%.15lf", a/b);
+    string result = s;
+
+    // cout<<s<<endl;
+    // for (int i = 0; i < 6; i++)
+    //     cout<<result[i];
+    // cout<<endl;
+
+    for (int i = 0; i < result.size(); i++)
+        if(result[i] != result[i-1] && i == result.size() - 1)
+            result[i] = result[i-1];
+
+    //cout<<result<<endl;
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        if(result[i] == '.')
+            for (int j = i + 1; j < result.size(); j++)
+            {
+                if(result[j] - '0' ==  c)
+                {
+                    cout<<j - i<<endl;
+                    f = true;
+                    break;
+                }
+            }
+            if(f)
+                break;
+    }
     
+    if(!f)
+        cout<<"-1"<<endl;
     return 0;
 }
