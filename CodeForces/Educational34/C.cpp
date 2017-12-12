88216eea64dd0fe18b0d61573854a50fecf2552f
SpiffyEight77 <killer_0248@live.com>
2017-12-13 00:37:15 +0800

    Update

--- CodeForces/Educational34/C.cpp
+++ CodeForces/Educational34/C.cpp
@@ -8,9 +8,30 @@ using namespace std;
 typedef long long ll;
 const int Maxn = 100500;
 const int Mod = 1000000000;
-int n;
+int n,ans,x,res,k,y,maxnn,cnt[Maxn],a[Maxn];
+bool f;
 int main()
 {
+    ans = 1;
+    maxnn = 1;
+    k = 0;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+        cin>>a[i];
     
+    sort(a,a+n);
+
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] == a[i+1])
+            ans++;
+        else
+        {
+            maxnn = max(ans,maxnn);
+            ans = 1;
+        }
+    }
+    
+    cout<<maxnn<<endl;
     return 0;
 }
