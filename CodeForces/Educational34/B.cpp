43528627409624b2a984454f243ae99a07ee5cf9
SpiffyEight77 <killer_0248@live.com>
2017-12-12 23:19:16 +0800

    Update

---
+++ CodeForces/Educational34/B.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+#include<iostream>
+#include <stdlib.h>  
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+const int Maxn = 100500;
+const int Mod = 1000000000;
+int n,x,y;
+int main()
+{
+    return 0;
+}
