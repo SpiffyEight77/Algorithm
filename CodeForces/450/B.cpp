4aa99c59798b85fd485194b048d1889ec212023d
SpiffyEight77 <killer_0248@live.com>
2017-12-12 16:14:30 +0800

    Update

--- CodeForces/450/B.cpp
+++ CodeForces/450/B.cpp
@@ -8,46 +8,20 @@ using namespace std;
 typedef long long ll;
 const int Maxn = 100500;
 const int Mod = 1000000000;
-int x,y,k,b;
-double a,c;
-char s[Maxn];
-bool f;
+int a,b,c,p = -1;
 int main()
 {
     cin>>a>>b>>c;
-    //cout<<a<<endl;
-
-    sprintf(s, "%.15lf", a/b);
-    string result = s;
-
-    // cout<<s<<endl;
-    // for (int i = 0; i < 6; i++)
-    //     cout<<result[i];
-    // cout<<endl;
-
-    for (int i = 0; i < result.size(); i++)
-        if(result[i] != result[i-1] && i == result.size() - 1)
-            result[i] = result[i-1];
-
-    //cout<<result<<endl;
-
-    for (int i = 0; i < result.size(); i++)
+    for (int i = 1; i < Maxn; i++)
     {
-        if(result[i] == '.')
-            for (int j = i + 1; j < result.size(); j++)
-            {
-                if(result[j] - '0' ==  c)
-                {
-                    cout<<j - i<<endl;
-                    f = true;
-                    break;
-                }
-            }
-            if(f)
-                break;
+        a*=10;
+        if(a / b == c)
+        {
+            p = i;
+            break;
+        }
+        a%=b;
     }
-    
-    if(!f)
-        cout<<"-1"<<endl;
+    cout<<p<<endl;
     return 0;
 }
