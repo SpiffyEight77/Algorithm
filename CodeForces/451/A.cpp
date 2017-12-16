1051070ed4b57b60ee0634858039bcde6e45e5b2
SpiffyEight77 <killer_0248@live.com>
2017-12-16 18:58:30 +0800

    CodeForces Round #451 (Div. 2)

---
+++ CodeForces/451/A.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+using namespace std;
+const int Maxn = 100000 + 100;
+const int Mod = 1000000000 + 7;
+int n,a[Maxn];
+int main()
+{
+    return 0;
+}
