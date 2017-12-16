57730edb8e79be694757259a5c4358f9f4e7fceb
SpiffyEight77 <killer_0248@live.com>
2017-12-16 19:54:47 +0800

    ABC82

---
+++ Atcoder/ABC82/B.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+using namespace std;
+const int Maxn = 100000 + 100;
+const int Mod = 1000000000 + 7;
+int n,l,a[Maxn];
+string S;
+bool f = false;
+int main()
+{
+   
+    return 0;
+}
