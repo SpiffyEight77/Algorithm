0c623fc008264f2b7121edf1a90fd7c3c05b4eb2
SpiffyEight77 <killer_0248@live.com>
2017-12-16 20:36:05 +0800

    Atcoder Beginner 82

--- Atcoder/ABC82/A.cpp
+++ Atcoder/ABC82/A.cpp
@@ -8,8 +8,14 @@ const int Maxn = 100000 + 100;
 const int Mod = 1000000000 + 7;
 int n,l,a[Maxn];
 string S;
+int x,y;
 bool f = false;
 int main()
 {
+    cin>>x>>y;
+    if( (x+y) % 2 == 0 )
+        cout<<(x+y)/2<<endl;
+    else
+        cout<<(x+y)/2+1<<endl;
     return 0;
 }
--- Atcoder/ABC82/B.cpp
+++ Atcoder/ABC82/B.cpp
@@ -7,10 +7,31 @@ using namespace std;
 const int Maxn = 100000 + 100;
 const int Mod = 1000000000 + 7;
 int n,l,a[Maxn];
-string S;
+char s[Maxn],t[Maxn];
 bool f = false;
+bool cmp(char x,char y)
+{
+    return x < y;
+}
+
+bool cmp2(char x,char y)
+{
+    return x > y;
+}
 int main()
 {
-   
+    cin>>s;
+    getchar();
+    cin>>t;
+    l = strlen(s);
+    sort(s,s+l,cmp);
+
+    l = strlen(t);
+    sort(t,t+l,cmp2);
+
+    if(strcmp(s,t) < 0)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
     return 0;
 }
