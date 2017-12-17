9f5edc6097b49fa807af0c2c0d02365f1c2dbf82
SpiffyEight77 <killer_0248@live.com>
2017-12-17 13:22:38 +0800

    CodeForce Round #452 Div. 2

---
+++ CodeForces/452/A.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+const int Maxn = 100100;
+const int Mod = 1000000000 + 7;
+int n,a[Maxn];
+int main()
+{
+    return 0;
+}
