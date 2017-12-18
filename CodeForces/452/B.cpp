1212eb8dc63e9abdd1b4e940cf3db67c7d7f6d1d
SpiffyEight77 <killer_0248@live.com>
2017-12-18 09:05:36 +0800

    Update

--- CodeForces/452/B.cpp
+++ CodeForces/452/B.cpp
@@ -7,61 +7,29 @@ using namespace std;
 const int Maxn = 100100;
 const int Mod = 1000000000 + 7;
 int n,cnt,k,a[Maxn];
-int m[50] = {0,31,28,31,30,31,30,31,31,30,31,30,31,31,29,31,30,31,30,31,31,30,31,30,31,31,28,31,30,31,30,31,31,30,31,30,31,31,28,31,30,31,30,31,31,30,31,30,31,31};
+int m[50] = {31,28,31,30,31,30,31,31,30,31,30,31,31,29,31,30,31,30,31,31,30,31,30,31,31,28,31,30,31,30,31,31,30,31,30,31,31,28,31,30,31,30,31,31,30,31,30,31};
 bool f;
 int main()
 {
     cin>>n;
     for (int i = 0; i < n; i++)
         cin>>a[i];
+    
     for (int i = 0; i < 50; i++)
-    {  
-        if(a[0] == m[i])
-        {
-            cnt = 1;
-            for (int j = i+1,k = 1; j < 50; j++)
+    {
+        f = true;
+        for (int j = 0; j < n; j++)
+            if(a[j] != m[(i+j)%48])
             {
-                if(a[k++] != m[j])
-                    break;
-                cnt++;
-                if(cnt == n)
-                    break;
+                f = false;
+                break;
             }
+        if(f)
+        {
+            cout<<"Yes"<<endl;
+            return 0;
         }
-        if(cnt == n)
-            break;
     }
-
-    //cout<<cnt<<endl;
-
-    if(cnt == n)
-        cout<<"Yes"<<endl;
-    else
-        cout<<"No"<<endl;
+    cout<<"No"<<endl;
     return 0;
 }
-
-// int main()
-// {
-//     cin>>n;
-//     for (int i = 0; i < n; i++)
-//         cin>>a[i];
-    
-//     for (int i = 0; i < 50; i++)
-//     {
-//         f = true;
-//         for (int j = 0; j < n; j++)
-//             if(a[j] != m[i+j])
-//             {
-//                 f = false;
-//                 break;
-//             }
-//         if(f)
-//         {
-//             cout<<"Yes"<<endl;
-//             return 0;
-//         }
-//     }
-//     cout<<"No"<<endl;
-//     return 0;
-// }
