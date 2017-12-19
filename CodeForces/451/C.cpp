c2e6c84708d6be373d8d5e7064157c4126123835
SpiffyEight77 <killer_0248@live.com>
2017-12-19 23:25:50 +0800

    CodeForces Round 453 Div.2

--- CodeForces/451/C.cpp
+++ CodeForces/451/C.cpp
@@ -1,13 +1,51 @@
 #include<iostream>
 #include<cstdio>
-#include<algorithm>
 #include<cstring>
+#include<algorithm>
 #include<cmath>
+#include<map>
+#include<vector>
+#define Mod 1000000000 + 7
+#define Maxn 100000 + 100
+typedef long long ll;
 using namespace std;
-const int Maxn = 100000 + 100;
-const int Mod = 1000000000 + 7;
-int n,a[Maxn];
+bool cmp(int x,int y)
+{
+    return x < y;
+}
+
+int gcd(int x,int y)
+{
+    return 0;
+}
+
+ll quick_mod()
+{
+    return 0;
+}
+
+int n,num;
+string s,ss;
 int main()
 {
+    cin>>n;
+    map<string,set<string> > mps;
+    for (int i = 0; i < n; i++)
+    {
+            cin>>s>>num;
+            for (int j = 0; j < num; j++)
+            {
+                cin>>ss;
+                mps[s].insert(ss);
+            }
+    }
+
+    cout<<mps.size()<<endl;
+
+    
+
+
+    
+
     return 0;
 }
