2ca7cc351ef2489881c62ade34b2a99962d34a18
SpiffyEight77 <killer_0248@live.com>
2017-12-30 01:43:03 +0800

    Update

--- CodeForces/Good Bye 2017/A.cpp
+++ CodeForces/Good Bye 2017/A.cpp
@@ -21,8 +21,21 @@ ll quick_mod()
 {
     return 0;
 }
-
+string S;
+int ans;
+char s[10] = {'a','e','i','o','u'};
+bool f;
 int main()
 {
+    cin>>S;
+    ans = 0;
+    for (int i = 0; i < S.size(); i++)
+        for (int j = 0; j < 5; j++)
+            if(S[i] == s[j] || (S[i] == '1' || S[i] == '3' || S[i] == '5' || S[i] == '7' || S[i] == '9' )  )
+            {
+                ans++;
+                break;
+            }
+    cout<<ans<<endl;
     return 0;
 }
--- CodeForces/Good Bye 2017/B.cpp
+++ CodeForces/Good Bye 2017/B.cpp
@@ -22,7 +22,103 @@ ll quick_mod()
     return 0;
 }
 
+
+
+int n,m,sx,sy,ex,ey,ans,num,dir[4][2] = {1,0, -1,0, 0,-1, 0,1};
+string S;
+char s[55][55],mark[Maxn];
+
+void dfs(int x,int y)
+{
+    if(x == ex && y == ey)
+    {
+        for (int i = 0; i < S.size(); i++)
+            if()
+        ans++;
+        return ;
+    }   
+
+    for (int i = 0; i < 4; i++)
+    {
+        x+=dir[i][0];
+        y+=dir[i][1];
+        
+        if(s[x][y] != '#' && x >= 0 && x < n && y >= 0 && y < m)
+        {
+            if(i == 0)
+                mark[num] = 'U';
+            else
+                if(i == 1)
+                    mark[num] = 'D';
+            else
+                if(i == 2)
+                    mark[num] = 'L';
+            else
+                if(i == 3)
+                    mark[num] = 'R';
+            num++;
+            dfs(x,y);
+            num--;
+            mark[num] = ' ';
+        }
+    }
+
+
+    return ;
+}
+
 int main()
 {
+    cin>>n>>m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin>>s[i][j];
+            if(s[i][j] == 'S')
+                sx = i, sy = j;
+            if(s[i][j] == 'E')
+                ex = i, ey = j;
+        }
+        getchar();
+    }
+
+    cin>>S;
+
+    num = 0;
+
+    dfs(ex,ey);
+
+    cout<<ans<<endl;
+
     return 0;
-}
+}  
+
+// U D L R 
+// 0 1 2 3
+// 0 1 3 2
+// 0 2 1 3
+// 0 2 3 1
+// 0 3 1 2
+// 0 3 2 1
+
+// 1 0 2 3
+// 1 0 3 2
+// 1 2 0 3
+// 1 2 3 0
+// 1 3 0 2
+// 1 3 2 0
+
+// 2 0 1 3
+// 2 0 3 1
+// 2 1 0 3
+// 2 1 3 0
+// 2 3 0 1
+// 2 3 1 0
+
+// 3 0 1 2
+// 3 0 2 1
+// 3 1 0 2
+// 3 1 2 0
+// 3 2 0 1
+// 3 2 1 0
