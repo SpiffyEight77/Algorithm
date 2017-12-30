a80f5632a44e677fe35afeed86bbd6d69ee35c69
SpiffyEight77 <killer_0248@live.com>
2017-12-30 19:33:46 +0800

    ABC84

---
+++ Atcoder/ABC84/B.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#define Mod 1000000000 + 7
+#define Maxn 1000000 + 100
+typedef long long ll;
+using namespace std;
+bool cmp(int x,int y)
+{
+    return x < y;
+}
+
+int gcd(int x,int y)
+{
+    return 0;
+}
+
+ll quick_mod()
+{
+    return 0;
+}
+
+int main()
+{
+    return 0;
+}
