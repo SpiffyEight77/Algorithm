c132fe666017b7ba552e126876b916d24b82ab26
SpiffyEight77 <killer_0248@live.com>
2018-01-08 22:26:43 +0800

    Hello 2018

---
+++ CodeForces/Hello 2018/A.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#define Mod 1000000000 + 7
+#define Maxn 1000000 + 100
+using ll = long long;
+using ld = long double;
+using namespace std;
+bool cmp(int x,int y)
+{
+    return x < y;
+}
+
+int gcd(int x,int y)
+{
+    return 0;
+}
+
+ll quick_mod()
+{
+    return 0;
+}
+
+int main()
+{
+    
+    return 0;
+}
