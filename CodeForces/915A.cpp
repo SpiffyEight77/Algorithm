a40cddf98f2b2636e91153a41a8f7e66241cd776
SpiffyEight77 <killer_0248@live.com>
2018-01-16 14:22:50 +0800

    915A

---
+++ CodeForces/915A.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#define Mod 1000000000 + 7
+#define Maxn 1000000 + 100
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+bool cmp(int x,int y)
+{
+    return x < y;
+}
+
+int gcd(int x,int y)
+{
+    return 0;
+}
+ 
+ll quick_mod()
+{
+    return 0;
+}
+int n,k,x,ans,a[Maxn];
+int main()
+{
+    ans = Mod;
+    cin>>n>>k;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>x;
+        if(k % x == 0)
+            ans = min(k / x,ans);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
