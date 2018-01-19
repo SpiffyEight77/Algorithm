897e1142858f59960ecfc9791c517506898aa2b1
SpiffyEight77 <killer_0248@live.com>
2018-01-19 15:46:17 +0800

    810A

--- CodeForces/810A.cpp
+++ CodeForces/810A.cpp
@@ -4,31 +4,25 @@
 #include<cmath>
 #define Maxn 100000
 using namespace std;
-int n,k,cnt,a[Maxn];
-double ave;
+int n,k,cnt,x;
+double sum;
 int main()
 {
     cin>>n>>k;
-    ave = 0;
-    cnt = 0;
+    sum = 0; cnt = 0;
     for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
-        ave+=a[i];
+        cin>>x;
+        sum+=x;
     }
 
-    if(round(ave/n) >= k)
-        cout<<"0"<<endl;
-    else
-        for (int i = 1; ; i++)
-        {
-            ave += k;
-            cnt++;
-            if(round(ave / (n+cnt) ) >= k) 
-            {
-                cout<<cnt<<endl;
-                break;
-            }
-        }
+    while(round( sum / (n+cnt) ) < k )
+    {
+        sum+=k;
+        cnt++;
+    }
+
+    cout<<cnt<<endl;
+
     return 0;
 }
