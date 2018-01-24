5e6981cd51edb3bfad78ed39033560d700bac857
SpiffyEight77 <killer_0248@live.com>
2018-01-24 21:51:33 +0800

    915A

--- CodeForces/915A.cpp
+++ CodeForces/915A.cpp
@@ -1,37 +1,56 @@
+// #include<iostream>
+// #include<cstdio>
+// #include<cstring>
+// #include<algorithm>
+// #include<cmath>
+// #define Mod 1000000000 + 7
+// #define Maxn 1000000 + 100
+// typedef long long ll;
+// typedef long double ld;
+// using namespace std;
+// bool cmp(int x,int y)
+// {
+//     return x < y;
+// }
+
+// int gcd(int x,int y)
+// {
+//     return 0;
+// }
+ 
+// ll quick_mod()
+// {
+//     return 0;
+// }
+// int n,k,x,ans,a[Maxn];
+// int main()
+// {
+//     ans = Mod;
+//     cin>>n>>k;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin>>x;
+//         if(k % x == 0)
+//             ans = min(k / x,ans);
+//     }
+//     cout<<ans<<endl;
+//     return 0;
+// }
+
+
 #include<iostream>
 #include<cstdio>
-#include<cstring>
-#include<algorithm>
 #include<cmath>
-#define Mod 1000000000 + 7
-#define Maxn 1000000 + 100
-typedef long long ll;
-typedef long double ld;
 using namespace std;
-bool cmp(int x,int y)
-{
-    return x < y;
-}
-
-int gcd(int x,int y)
-{
-    return 0;
-}
- 
-ll quick_mod()
-{
-    return 0;
-}
-int n,k,x,ans,a[Maxn];
+int n,k,x,ans = 999;
 int main()
 {
-    ans = Mod;
     cin>>n>>k;
     for (int i = 0; i < n; i++)
     {
         cin>>x;
         if(k % x == 0)
-            ans = min(k / x,ans);
+            ans = min(ans,k / x);
     }
     cout<<ans<<endl;
     return 0;
