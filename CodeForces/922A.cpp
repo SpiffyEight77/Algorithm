692962c4f9ab12cc18f5c5a611cb02fdfecd3b63
SpiffyEight77 <killer_0248@live.com>
2018-02-08 23:49:26 +0800

    922A

---
+++ CodeForces/922A.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#define Maxn 100000
+using namespace std;
+int x,y;
+int main()
+{
+    cin>>x>>y;
+    
+    return 0;
+}
