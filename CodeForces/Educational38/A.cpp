d79343468660009be478e13976c15fe5576af8fa
SpiffyEight77 <killer_0248@live.com>
2018-02-16 23:22:31 +0800

    Update

---
+++ CodeForces/Educational38/A.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#define Maxn 100000 + 10
+using namespace std;
+string S;
+int n,j = 0,i = 1;
+int main()
+{
+    cin>>n;
+    cin>>S;
+    
+    cout<<endl;
+    return 0;
+}
---
+++ CodeForces/Educational38/B.cpp
@@ -0,0 +1,11 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#define Maxn 100000 + 10
+using namespace std;
+int main()
+{
+    return 0;
+}
