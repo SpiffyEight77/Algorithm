18ec0e6bcc1508a23f5794e71cc5334d73a9e0b4
SpiffyEight77 <killer_0248@live.com>
2018-02-17 12:26:16 +0800

    PAT1024

--- PAT/Basic/1024.cpp
+++ PAT/Basic/1024.cpp
@@ -6,32 +6,26 @@
 #define Maxn 1000100
 using namespace std;
 string S;
-bool f1 = true,f2 = true;
+bool f = true;
 int ans = 0,x;
 int main()
 {
     cin>>S;
     if(S[0] == '-')
-        f1 = false;
-    for (int i = 0; i < S.size(); i++)
+        cout<<"-";
+    for (int i = 1; i < S.size(); i++)
     {
         if(S[i] == 'E')
         {
              x = i - 1;
              if(S[i+1] == '-')
-                f2 = false;
+                f = false;
             for (int j = i + 2; j < S.size(); j++)
                 ans = ans * 10 + (S[j] - '0');
             break;
         }
     }
-    if(!f1)
-        cout<<"-";
-    if(ans == 0)
-        for (int i = 1; i <= x; i++)
-            cout<<S[i];
-    else
-    if(!f2)
+    if(f == false)
     {   
         cout<<"0.";
         for (int i = 1; i <= ans - 1; i++)
@@ -42,11 +36,23 @@ int main()
     }
     else
     {
-        for (int i = 1; i <= x; i++)
-            if(S[i] != '.')
+        if(ans >= x - 2)
+        {
+            for (int i = 1; i <= x; i++)
+                if(S[i] != '.')
+                    cout<<S[i];
+            for (int i = 1; i <= ans + 2 - x; i++)
+                cout<<"0";
+        }
+        else
+        {
+            cout<<S[1];
+            for (int i = 3; i < 3 + ans; i++)
                 cout<<S[i];
-        for (int i = 1; i <= ans + 1 - x; i++)
-            cout<<"0";
+            cout<<".";
+            for (int i = 3 + ans; i <= x; i++)
+                cout<<S[i];
+        }
     }
     cout<<endl;
     return 0;
