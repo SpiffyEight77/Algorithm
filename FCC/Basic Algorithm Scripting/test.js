c3a9b98404eb0edac6a4d0c4e0a893b555d8ab26
SpiffyEight77 <killer_0248@live.com>
2018-06-24 21:34:58 +0800

    Update FCC Basic Algorithm Scripting

--- FCC/Basic Algorithm Scripting/test.js
+++ FCC/Basic Algorithm Scripting/test.js
@@ -1,19 +1,12 @@
-function largestOfFour(arr) {
-    
-    var a = new Array(arr.length);
-
-    for (var i = 0; i < arr.length; i++) {
-        var res = arr[i][0];
-        for (var j = 1; j < arr[i].length; j++) {
-            if (res < arr[i][j])
-                res = arr[i][j];
-        }
-        a[i] = res;
-    }
-
-    return a;
+function confirmEnding(str, target) {
+   for (var i = str.length - 1; i >= 0; i--) {
+       if (str.substring(i,str.length) === target) {
+           return true;
+       }
+   }
+    return false;
 }
   
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
-
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+  confirmEnding("Bastian", "n");
+  
+  console.log( confirmEnding("Bastian", "n"));
